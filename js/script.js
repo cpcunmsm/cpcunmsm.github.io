b2e839239e703f8ea3e5b6ab7be591ef23ce0d3b
Jorge Ktch <jorgektch@gmail.com>
2024-08-01 18:40:02 -0500

    Add content

--- js/script.js
+++ js/script.js
@@ -1,29 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Actualizar el año en el pie de páginas
+    // Actualizar el año en el pie de página
     document.getElementById('year').textContent = new Date().getFullYear();
 
     // Manejo del menú desplegable en dispositivos móviles
     const menuToggle = document.querySelector('.menu-toggle');
     const navMenu = document.querySelector('nav');
-    const dropdowns = document.querySelectorAll('.dropdown');
+    const navLinks = document.querySelectorAll('nav ul li a');
+    const header = document.querySelector('header');
+    const headerHeight = header.offsetHeight;
 
     menuToggle.addEventListener('click', function () {
         navMenu.classList.toggle('active');
         menuToggle.classList.toggle('close');
     });
 
-    dropdowns.forEach(dropdown => {
-        dropdown.addEventListener('click', function (event) {
-            event.stopPropagation();
-            dropdown.classList.toggle('active');
-        });
-    });
+    // Cerrar el menú al hacer clic en un enlace y ajustar el desplazamiento
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault(); // Evitar el comportamiento predeterminado
 
-    document.addEventListener('click', function (event) {
-        if (!event.target.closest('nav') && !event.target.closest('.menu-toggle')) {
+            // Obtener el ID de la sección a la que se quiere desplazar
+            const targetId = link.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            // Calcular la posición ajustada teniendo en cuenta la altura del header
+            const offsetTop = targetSection.offsetTop - headerHeight;
+
+            // Desplazarse a la posición ajustada
+            window.scrollTo({
+                top: offsetTop,
+                behavior: 'smooth'
+            });
+
+            // Cerrar el menú móvil
             navMenu.classList.remove('active');
             menuToggle.classList.remove('close');
-            dropdowns.forEach(dropdown => dropdown.classList.remove('active'));
-        }
+        });
     });
 });
