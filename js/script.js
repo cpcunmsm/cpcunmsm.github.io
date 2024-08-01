fc0b69111da81609e857ab528708fecbfe1298a4
Jorge Ktch <jorgektch@gmail.com>
2024-08-01 17:28:39 -0500

    Add content

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
-// Actualizar el año en el pie de página
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    // Actualizar el año en el pie de páginas
     document.getElementById('year').textContent = new Date().getFullYear();
+
+    // Manejo del menú desplegable en dispositivos móviles
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navMenu = document.querySelector('nav');
+    const dropdowns = document.querySelectorAll('.dropdown');
+
+    menuToggle.addEventListener('click', function () {
+        navMenu.classList.toggle('active');
+        menuToggle.classList.toggle('close');
+    });
+
+    dropdowns.forEach(dropdown => {
+        dropdown.addEventListener('click', function (event) {
+            event.stopPropagation();
+            dropdown.classList.toggle('active');
+        });
+    });
+
+    document.addEventListener('click', function (event) {
+        if (!event.target.closest('nav') && !event.target.closest('.menu-toggle')) {
+            navMenu.classList.remove('active');
+            menuToggle.classList.remove('close');
+            dropdowns.forEach(dropdown => dropdown.classList.remove('active'));
+        }
+    });
 });
